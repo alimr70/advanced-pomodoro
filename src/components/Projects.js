6f32b38fcb900acf71b1c402c614ccc3464cc0cd
Ali Maher <ali.maher.mohammed@gmail.com>
2021-07-02 17:00:12 +0200

    finished structrure of Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ const Projects = () => {
       </header>
 
       {/* Main section - working session time options buttons */}
-      <main className="bg-gray-800 py-1 row-span-6">
+      <main className="bg-gray-800 py-1 row-span-6 relative flex flex-col">
         <div className="bg-gray-900 my-px flex items-center justify-center font-normal">
           {/* Stars */}
           <div className="p-3">
@@ -61,6 +61,19 @@ const Projects = () => {
               />
             </svg>
           </div>
+        </div>
+        <div className="p-5 self-center mt-auto bg-green-500 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7 text-gray-100"
+            viewBox="0 0 20 20"
+            fill="currentColor">
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+              clipRule="evenodd"
+            />
+          </svg>
         </div>
       </main>
     </>
