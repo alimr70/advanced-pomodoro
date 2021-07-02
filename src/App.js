6ff1ae5197ff0c9646f4d61e7f90463dcc944525
Ali Maher <ali.maher.mohammed@gmail.com>
2021-07-02 04:52:18 +0200

    finished main screen component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App bg-gray-900 h-full xs:h-screen grid xs:grid-rows-7">
+    <div className="App text-green-400 text-2xl font-bold bg-gray-900 h-full xs:h-screen grid xs:grid-rows-7">
       <Main />
     </div>
   );
