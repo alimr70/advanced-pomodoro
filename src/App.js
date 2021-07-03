d36f815be5505c80fa002e966f7190a360bc349f
Ali Maher <ali.maher.mohammed@gmail.com>
2021-07-03 07:13:56 +0200

    finished structrure of Settings component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import AddSession from "./components/AddSession";
 import SessionsDay from "./components/SessionsDay";
 import Sessions from "./components/Sessions";
 import Projects from "./components/Projects";
+import Settings from "./components/Settings";
 
 function App() {
   return (
     <div className="App text-green-400 text-2xl font-bold bg-gray-900 h-full grid">
-      <Projects />
+      <Settings />
     </div>
   );
 }
